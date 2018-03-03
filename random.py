9847d2beec73dd09059e50c897f56ba6192d55bc
PLSousa <paulo.loureiro.de.sousa@gmail.com>
2018-03-03 19:09:01 +0100

    Add files via upload

---
+++ random.py
@@ -0,0 +1,34 @@
+"""
+usage:
+>>> from random import randint
+>>> randint(1,10)
+>>> 
+>>> from random import choice
+>>> a = ['Paris','London','Roma','Rio']
+>>> choice(a)
+"""
+
+# Pseudo-random number generator module
+
+def _bytes_to_int(bytes):
+    result = 0
+    for b in bytes:
+        result = result * 256 + int(b)
+    return result
+
+def _rescale(Nmin, Nmax, OldValue):
+    NewRange = Nmax - Nmin  
+    NewValue = OldValue * NewRange / 255 + Nmin
+    return NewValue
+
+# Return a random integer N such that Nmin <= N <= Nmax.
+def randint(Nmin, Nmax):
+    from uos import urandom
+    Oldint = _bytes_to_int(urandom(1))
+    N = round(_rescale(Nmin-0.5, Nmax+0.5, Oldint))
+    return N
+
+# Return a random element from a list.
+def choice(mylist):
+    index = randint(0,len(mylist)-1)
+    return mylist[index]
